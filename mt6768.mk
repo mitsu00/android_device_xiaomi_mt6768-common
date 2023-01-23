8a451576c2f95d28ef2add71a3b90b07e6be96d1
Jaroslav Zviezda <acroreiser@gmail.com>
2023-01-24 01:59:15 +0800

    mt6768-common: Enable DM file pre-opting to reduce first boot time

--- mt6768.mk
+++ mt6768.mk
@@ -205,6 +205,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Enable DM file pre-opting to reduce first boot time
+PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
+
 # Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
